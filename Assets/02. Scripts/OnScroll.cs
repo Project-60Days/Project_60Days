c8697ec80a591a66b15f905359ef29dbe25f5272
dah <daun792@gmail.com>
2023-04-05 00:47:33 +0900

    [Add] 마우스 휠로 맵 이동 기능 구현
    ori의 맵 전환 방식 참고 (줌인/줌아웃, 페이드인/페이드아웃)

---
+++ Assets/02. Scripts/OnScroll.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class OnScroll : MonoBehaviour
+{
+    public Transform[] panels;
+    public Image[] images;
+
+    private int currentPanel = 0;
+
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll > 0f) // 마우스 휠을 위로 스크롤한 경우
+        {
+            if (currentPanel > 0)
+            {
+                currentPanel--; // 이전 패널로 이동
+
+                Sequence sequence = DOTween.Sequence();
+                sequence.Append(panels[currentPanel + 1].DOScale(1.5f, 1f).SetEase(Ease.InQuad))
+                    .Append(images[currentPanel + 1].DOFade(0f, 0.5f))
+                    .Join(images[currentPanel].DOFade(1f, 0.5f))
+                    .Join(panels[currentPanel].DOScale(1.25f, 0.01f));
+
+                panels[currentPanel + 1].DOKill();
+                panels[currentPanel].DOKill();
+
+                sequence.Play();
+
+            }
+        }
+        else if (scroll < 0f) // 마우스 휠을 아래로 스크롤한 경우
+        {
+            if (currentPanel < panels.Length - 1)
+            {
+                currentPanel++; // 다음 패널로 이동
+
+                Sequence sequence = DOTween.Sequence();
+                sequence.Append(panels[currentPanel - 1].DOScale(1f, 1f).SetEase(Ease.InQuad))
+                    .Append(images[currentPanel - 1].DOFade(0f, 0.5f))
+                    .Join(images[currentPanel].DOFade(1f, 0.5f))
+                    .Join(panels[currentPanel].DOScale(1.25f, 0.01f));
+                
+                panels[currentPanel - 1].DOKill();
+                panels[currentPanel].DOKill();
+
+                sequence.Play();
+            }
+        }
+    }
+}
