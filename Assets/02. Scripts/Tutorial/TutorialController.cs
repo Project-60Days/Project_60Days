ee9da8d428826bae975ccd75aa7eea36d8072c9c
MakerJun46 <guswns6711@naver.com>
2023-07-20 21:30:21 +0900

    [Add] : TutorialManager.cs 추가

---
+++ Assets/02. Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialController : MonoBehaviour
+{
+    List<TutorialBase> tutorialList;
+
+    TutorialBase currentTutorial;
+    int TutorialIndex = -1;
+
+    public void Init()
+    {
+        tutorialList = new List<TutorialBase>(this.GetComponentsInChildren<TutorialBase>());
+
+        SetNextTutorial();
+    }
+
+    private void Update()
+    {
+        currentTutorial?.Execute(this);
+    }
+
+    public void SetNextTutorial()
+    {
+        currentTutorial?.Exit();
+
+        if (TutorialIndex >= tutorialList.Count - 1)
+        {
+            CompleteTutorial();
+            return;
+        }
+
+        TutorialIndex++;
+        currentTutorial = tutorialList[TutorialIndex];
+
+        currentTutorial?.Enter();
+    }
+
+    void CompleteTutorial()
+    {
+        currentTutorial = null;
+
+        SceneManager.LoadScene("Scene_01_Lobby");
+    }
+}
