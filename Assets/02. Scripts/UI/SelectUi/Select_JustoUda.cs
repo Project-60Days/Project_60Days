1d9e1025cd37cec7e6dc8b0253effe2d35208f88
dah <daun792@gmail.com>
2023-09-03 22:44:06 +0900

    [Add] SelectUi 생성

---
+++ Assets/02. Scripts/UI/SelectUi/Select_JustoUda.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Select_JustoUda : SelectBase
+{
+    [SerializeField] Sprite imageA;
+    [SerializeField] Sprite imageB;
+    [SerializeField] string textA;
+    [SerializeField] string textB;
+
+    public Select_JustoUda()
+    {
+        key = "JustoUda";
+    }
+
+    public override void SelectA()
+    {
+        Debug.Log("A 버튼 눌림");
+        gameObject.SetActive(false);
+    }
+
+    public override void SelectB()
+    {
+        Debug.Log("B 버튼 눌림");
+        gameObject.SetActive(false);
+    }
+
+    public override void GetOptionA(Button button)
+    {
+        button.GetComponent<Image>().sprite = imageA;
+        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = textA;
+    }
+
+    public override void GetOptionB(Button button)
+    {
+        button.GetComponent<Image>().sprite = imageB;
+        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = textB;
+    }
+}
