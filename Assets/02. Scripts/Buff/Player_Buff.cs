612f5579eb2870dc68f63a095c8fb035e6bfb110
dah <daun792@gmail.com>
2024-01-08 00:21:26 +0900

    [Add] PlayerBuff 스크립트 추가

---
+++ Assets/02. Scripts/Buff/Player_Buff.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Buff : MonoBehaviour
+{
+    void AddMoveRange(int _amout)
+    {
+
+    }
+
+    void AddMoveRangeUntil(int _duration, int _amount)
+    {
+
+    }
+
+    void AddDurability(int _duration, int durability)
+    {
+
+    }
+
+    void AddPurseBomb(int _amount)
+    {
+
+    }
+
+    void MoveToStruct(string _structName)
+    {
+
+    }
+
+    void GetItemN(string _itemCode, int _amount)
+    {
+
+    }
+
+    void ClockUntil(int _duration)
+    {
+
+    }
+
+    void GetSightOn(string Area)
+    {
+
+    }
+
+    void AddSightRange(int _amount)
+    {
+
+    }
+}
