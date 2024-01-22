11e5e47135b33689eece433abeb86f6bb1c80498
dah <daun792@gmail.com>
2024-01-22 16:49:29 +0900

    [Mod] 설계도 기능 구현 완료

---
+++ Assets/02. Scripts/UI/CraftUi/Slot/Slot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : SlotBase
+{
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        return;
+    }
+}
