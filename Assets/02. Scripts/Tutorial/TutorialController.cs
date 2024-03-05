d048a16bb4323673b23eb170da7b7ea41cb35a7c
dah <daun792@gmail.com>
2024-03-05 09:16:27 +0900

    [Mod]: 튜토리얼 오브젝트 lightUp 방식 변경

--- Assets/02. Scripts/Tutorial/TutorialController.cs
+++ Assets/02. Scripts/Tutorial/TutorialController.cs
@@ -15,9 +15,9 @@ public class TutorialController : MonoBehaviour
     [SerializeField] Button backToBaseBtn;
 
     Image lightBackground;
-    Image workBenchImage;
+    CanvasGroup workBenchImage;
     Image batteryImage;
-    Image mapImage;
+    CanvasGroup mapImage;
     CanvasGroup workBenchDeco;
 
     WorkBenchInteraction workBenchScript;
@@ -33,9 +33,9 @@ public class TutorialController : MonoBehaviour
     {
         lightBackground = GameObject.FindWithTag("LightImage").GetComponent<Image>();
         lightBackground.gameObject.SetActive(false);
-        workBenchImage = GameObject.FindWithTag("WorkBench").GetComponent<Image>();
+        workBenchImage = GameObject.FindWithTag("WorkBench").GetComponent<CanvasGroup>();
         batteryImage = GameObject.FindWithTag("Battery").GetComponent<Image>();
-        mapImage = GameObject.FindWithTag("Map").GetComponent<Image>();
+        mapImage = GameObject.FindWithTag("Map").GetComponent<CanvasGroup>();
         workBenchDeco = GameObject.FindWithTag("WorkBenchDeco").GetComponent<CanvasGroup>();
         workBenchScript = workBenchImage.GetComponent<WorkBenchInteraction>();
 
@@ -92,30 +92,25 @@ public class TutorialController : MonoBehaviour
 
     public void LightUpWorkBench()
     {
-        Color color = new Color(0.4f, 0.4f, 0.4f, 1f);
-        workBenchImage.DOColor(color, 0f).OnComplete(() => workBenchImage.transform.SetAsLastSibling());
-
-        
+        workBenchImage.DOFade(0.4f, 0f).OnComplete(() => workBenchImage.transform.SetAsLastSibling());
     }
+
     public void LightDownWorkBench()
     {
         workBenchImage.transform.SetSiblingIndex(workBenchInitIndex);
 
-        Color color = new Color(1f, 1f, 1f, 1f);
-        workBenchImage.DOColor(color, 0f);
+        workBenchImage.DOFade(1f, 0f);
     }
 
     public void LightUpMap()
     {
-        Color color = new Color(0.6f, 0.6f, 0.6f, 1f);
-        mapImage.DOColor(color, 0f).OnComplete(() => mapImage.transform.SetAsLastSibling());
+        mapImage.DOFade(0.6f, 0f).OnComplete(() => mapImage.transform.SetAsLastSibling());
     }
     public void LightDownMap()
     {
         mapImage.transform.SetSiblingIndex(mapInitIndex);
 
-        Color color = new Color(1f, 1f, 1f, 1f);
-        mapImage.DOColor(color, 0f);
+        mapImage.DOFade(1f, 0f);
     }
 
     public void LightUpBackground()
